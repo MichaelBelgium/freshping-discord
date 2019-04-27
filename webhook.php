8bf7cdc5fe0b091c4f12869e41bef3e178a71739
Michael V <michaelvrld@gmail.com>
2019-04-27 19:49:20 +0200

    Send message to discord

--- webhook.php
+++ webhook.php
@@ -4,6 +4,23 @@ include __DIR__.'/vendor/autoload.php';
 
 use RestCord\DiscordClient;
 
-define("DISCORD_TOKEN", "");
-define("DISCORD_CHANNEL", "");
+define("DISCORD_TOKEN", null);
+define("DISCORD_CHANNEL", null);
+
+$json_data = file_get_contents("php://input");
+
+if (empty($json_data) || empty(DISCORD_CHANNEL) || empty(DISCORD_TOKEN))
+    die();
+
+$response = new FreshpingResponse($json_data);
+$client = new DiscordClient(['token' => DISCORD_TOKEN]);
+
+$message = ":bell: **" . $response->getWebhookEventData()->getCheckName() . ":** " . 
+           $response->getWebhookEventData()->getCheckStateName() . " (" .
+           $response->getWebhookEventData()->getRequestStartTime()->format('Y-m-d H:m:i') . ")";
+
+$client->channel->createMessage([
+    'channel.id' => DISCORD_CHANNEL,
+    'content' => $message
+]);
 ?>
