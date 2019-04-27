222b800c14da833fcb332373d4bce84c3e8d2014
Michael V <michaelvrld@gmail.com>
2019-04-27 18:26:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Freshping to discord
---
+++ webhook.php
@@ -0,0 +1,9 @@
+<?php
+
+include __DIR__.'/vendor/autoload.php';
+
+use RestCord\DiscordClient;
+
+define("DISCORD_TOKEN", "");
+define("DISCORD_CHANNEL", "");
+?>
